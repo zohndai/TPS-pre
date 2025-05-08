2a0cccea521d88c90a56603e45141d903d7e06c3
zohndai <zohndai@gmail.com>
2025-05-08 19:24:23 +0800

    Update 01_making_predictions.py

--- pages/01_making_predictions.py
+++ pages/01_making_predictions.py
@@ -58,7 +58,7 @@ def download():
 	#1fa4ErVvXjZzbLCZzJEsQ6I9nelAR7iCu
 	#1NmXn3OhaAexvfhdTwZQxq8gbKaF_GV6u
 	fd_dict = {'1NTYBl070koP1fglDnTkLPxOOP94ehlAv':f'{name}_2025_0508'}
-	https://drive.google.com/file/d/1NTYBl070koP1fglDnTkLPxOOP94ehlAv/view?usp=sharing
+	#https://drive.google.com/file/d/1NTYBl070koP1fglDnTkLPxOOP94ehlAv/view?usp=sharing
 	for fd in fd_dict.keys():
 		fd_file = fd
 		model_name = fd_dict[fd]
