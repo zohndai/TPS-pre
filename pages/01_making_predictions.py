25b687580187176e3b65e92687fb3fcf3619a209
zohndai <zohndai@gmail.com>
2025-05-13 18:46:23 +0800

    Update 01_making_predictions.py

--- pages/01_making_predictions.py
+++ pages/01_making_predictions.py
@@ -1,6 +1,6 @@
 import streamlit as st
-# import streamlit_ketcher as sk
-import streamlit.components.v1 as components
+from streamlit_ketcher import st_ketcher
+# import streamlit.components.v1 as components
 import pandas as pd
 import numpy as np
 import rdkit
