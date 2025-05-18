6f39ff05936f7b80c16fba023dc61dfc4a3a2522
zohndai <zohndai@gmail.com>
2025-05-18 16:42:52 +0800

    Update 01_making_predictions.py

--- pages/01_making_predictions.py
+++ pages/01_making_predictions.py
@@ -282,7 +282,7 @@ def run():
 		for topi, smi in zip([f"top{i}" for i in range(1,11)], smis_li):
 			message_container.markdown(
 				"<br>".join([
-					f"**topi:** `{smi}`" for i in range(1,11)]), 
+					f"**{topi}:** `{smi}`" for i in range(1,11)]), 
 				unsafe_allow_html=True
 			)
 		# st.markdown(",".join([f"**top{i}:** `{smis_li[i-1]}`" for i in range(1,11)])) 这行速度太慢了
