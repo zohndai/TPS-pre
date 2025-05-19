b9dc126456a35d84c0a8a51e174fbac824b43004
zohndai <zohndai@gmail.com>
2025-05-20 03:28:16 +0800

    Update 01_making_predictions.py

--- pages/01_making_predictions.py
+++ pages/01_making_predictions.py
@@ -59,6 +59,7 @@ def download():
 	#https://drive.google.com/file/d/1MTK_uL2hyS2QJrmq4HjaVCdJNLkTf55s/view?usp=drive_link
 	#1fa4ErVvXjZzbLCZzJEsQ6I9nelAR7iCu
 	#1NmXn3OhaAexvfhdTwZQxq8gbKaF_GV6u
+	# https://drive.google.com/file/d//view?usp=sharing
 	fd_dict = {'1NTYBl070koP1fglDnTkLPxOOP94ehlAv':f'{name}_2025_0508'}
 	#https://drive.google.com/file/d/1NTYBl070koP1fglDnTkLPxOOP94ehlAv/view?usp=sharing
 	for fd in fd_dict.keys():
