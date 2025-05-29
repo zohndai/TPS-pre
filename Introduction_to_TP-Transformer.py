2594ab1b4ccb441c7d80fecaa506dc639167d1a5
zohndai <zohndai@gmail.com>
2025-05-29 10:27:57 +0800

    Update Introduction_to_TP-Transformer.py

--- Introduction_to_TP-Transformer.py
+++ Introduction_to_TP-Transformer.py
@@ -4,11 +4,6 @@ import pandas as pd
 from PIL import Image
 import streamlit.components.v1 as components
 
-
-
-
-
-
 st.set_page_config(
     page_title="Welcome to TP-Transformer",    
     page_icon="log.ico",        
@@ -16,10 +11,10 @@ st.set_page_config(
     initial_sidebar_state="auto" 
 )
 
-st.sidebar.markdown("## Contact us")
-st.sidebar.markdown("📧 Email: zhen.h.dai@outlook.com")
-st.sidebar.markdown("📱 Wechat: your_wechat_id")
-st.sidebar.markdown("[feedback](https://example.com)")
+st.sidebar.markdown("## 📞Contact us")
+st.sidebar.markdown("📬 Email: zhen.h.dai@outlook.com")
+st.sidebar.markdown("📱 Wechat: Zohn-wehcat")
+st.sidebar.markdown("[feedback](https://docs.qq.com/form/page/DVFdraEFYeEdCZEJ6)")
 
 visitor = pd.read_csv("visi_num.txt")
 visi_num = visitor['num'][0]
@@ -32,26 +27,6 @@ st.session_state.visitor_count += 1
 visitor['num'][0] += 1
 st.metric(label=f'👀page views: {visi_num}', value='')
 visitor.to_csv("visi_num.txt", index=False)
-
-
-if st.button("📬 联系开发者"):
-    with st.modal("📞 联系开发者"):
-        st.markdown("您可以通过以下方式联系我：")
-
-        col1, col2 = st.columns([1, 4])
-        with col1:
-            st.image("https://img.icons8.com/ios-glyphs/30/000000/new-post.png", width=24)
-        with col2:
-            st.markdown("[发送邮件](zhen.h.dai@outlook.com)")
-
-        col3, col4 = st.columns([1, 4])
-        with col3:
-            st.image("https://img.icons8.com/external-flat-juicy-fish/60/000000/external-form-ux-and-ui-flat-flat-juicy-fish.png", width=24)
-        with col4:
-            st.markdown("[填写在线反馈表单](https://docs.qq.com/form/page/DVFdraEFYeEdCZEJ6)")
-
-        st.markdown("感谢您的反馈！我们会尽快回复。")
-
 
 TEXT1 = """
         <body style='text-align: justify; color: black;'>
