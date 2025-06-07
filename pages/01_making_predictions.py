2f7840c271d0afaf449ed603eba09a1e808c952c
zohndai <zohndai@gmail.com>
2025-06-08 01:39:40 +0800

    Update 01_making_predictions.py

--- pages/01_making_predictions.py
+++ pages/01_making_predictions.py
@@ -252,15 +252,15 @@ with st.sidebar:
 			thred_cols = thed_cols1 if i < 3 else thed_cols2
 			j = i if i < 3 else i-3
 			with thred_cols[j]:
-			thresholds[i] = st.number_input(
-			    label=f"top{i+1} threshold:",
-			    min_value=0.0,
-			    max_value=1.0,
-			    value=default_values[i],
-			    format="%.9f",
-			    step=1e-9,
-			    help=f"default: {default_values[i]:.9f}"
-			)
+				thresholds[i] = st.number_input(
+				    label=f"top{i+1} threshold:",
+				    min_value=0.0,
+				    max_value=1.0,
+				    value=default_values[i],
+				    format="%.9f",
+				    step=1e-9,
+				    help=f"default: {default_values[i]:.9f}"
+				)
 
 
 
