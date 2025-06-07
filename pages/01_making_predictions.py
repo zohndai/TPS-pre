25c568b4c85b65e0890055cff5e1a993eaebbdf3
zohndai <zohndai@gmail.com>
2025-06-08 01:38:18 +0800

    Update 01_making_predictions.py

--- pages/01_making_predictions.py
+++ pages/01_making_predictions.py
@@ -241,22 +241,26 @@ with st.sidebar:
 		# "+" and "-" buttons for fine-tuning
 	
 		st.subheader('⚖️Specify probability thresholds')
-		cols = st.columns(5)
+		thed_cols1 = st.columns(5)
+		thed_cols2 = st.columns(5)
 	
 		default_values = get_thred(ros_selct, methd_selct)
 		
 		thresholds = {}
+		
 		for i in range(5):
-		    with cols[i]:
-		        thresholds[i] = st.number_input(
-		            label=f"top{i+1} threshold:",
-		            min_value=0.0,
-		            max_value=1.0,
-		            value=default_values[i],
-		            format="%.9f",
-		            step=1e-9,
-		            help=f"default: {default_values[i]:.9f}"
-		        )
+			thred_cols = thed_cols1 if i < 3 else thed_cols2
+			j = i if i < 3 else i-3
+			with thred_cols[j]:
+			thresholds[i] = st.number_input(
+			    label=f"top{i+1} threshold:",
+			    min_value=0.0,
+			    max_value=1.0,
+			    value=default_values[i],
+			    format="%.9f",
+			    step=1e-9,
+			    help=f"default: {default_values[i]:.9f}"
+			)
 
 
 
