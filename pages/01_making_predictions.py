27996c00765933411df8f3b8e39efadf45d79325
zohndai <zohndai@gmail.com>
2025-06-08 01:29:31 +0800

    Update 01_making_predictions.py

--- pages/01_making_predictions.py
+++ pages/01_making_predictions.py
@@ -279,7 +279,7 @@ def run():
 	# molecule = st.text_input("molecular strcuture（SMILES）", default_smiles)
 	# Ketcher  molecule editor
 	st.markdown(f"✍Or manually draw the target pollutant below:")
-	poll = st_ketcher(default_mol,height=350)
+	poll = st_ketcher(default_mol,height=550)
 	# Showing molecule SMILES from editor
 	st.markdown(f"**current SMILES：** `{poll}`")
 
